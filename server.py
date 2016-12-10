cf59adf61b03aca0746d8e24e125ffbca84060a9
PavelTertychnyi <pavel.tertychny@gmail.com>
2016-12-10 17:10:10 +0200

    Class patterns
    Added class patterns

--- server.py
+++ server.py
@@ -1,2 +1,61 @@
-import pika
-import os
+import uuid
+from Queue import Queue
+
+class GameSession:
+    def __init__(self, id):
+        self.id = uuid.uuid4()
+        self.ships = []
+        self.players = Queue()
+        self.master_client_id = id
+
+    def disconnect(self, player_id):
+        return 0
+
+    def leave(self, player_id):
+        return 0
+
+class Wait(GameSession):
+    def addPayer(self, login, id):
+        player = Player(login, id)
+        self.players.put(player)
+
+    def addShipsOfPlayer(self, id, ships):
+
+        return 0
+
+    def sendStats(self):
+        return 0
+
+    def startGame(self):
+        return 0
+
+    def receiveStartGame(self):
+        self.startGame()
+
+class Game(GameSession):
+    def currentActivePlayers(self):
+        return 0
+
+    def makeHit(self):
+        return 0
+
+    def sendStats(self):
+        return 0
+
+    def checkEndGame(self):
+        return 0
+
+class Ship:
+    def __init__(self, length, start, direction):
+        self.length = length
+        self.coordinates = []
+
+class Player:
+    def __init__(self, login, id):
+        self.id = id
+        self.login = login
+        self.score = 0
+        self.type = 'Player'  # Spectator, Leaved
+
+game = GameSession(1)
+print game.id
