a7859460f72ebe8f6940959d7927c42d15b482c3
islobozhan <ivan.slobozhan@gmail.com>
2016-12-12 17:48:36 +0200

    New

--- battle_login.py
+++ battle_login.py
@@ -1,27 +1,103 @@
 from Tkinter import *
-
+from battle_client import *
+from battle_board import *
+import threading
+import tkMessageBox
 
 class Login(Frame):
-    def __init__(self, root):
+    def __init__(self, root, server_id='127.0.0.1'):
+
+        self.server_id = server_id
+
+        self.client = Client(self.server_id)
+
         self.root = root
-        #self.initUI()
-        #self.get_nickname()
+
         self.root.title("Battleships!")
 
-    def initUI(self):
+    def choose_server(self):
 
-        # Return the chosen game server
-        def choose_game():
+        avaliable_servers_frame = Frame(self.root)
+        avaliable_servers_frame.grid(row=0, column=0)
+
+        avaliable_servers = Listbox(avaliable_servers_frame, width=20,
+                                    height=10, font=10, bg='ghost white', activestyle='none')
+        avaliable_servers.grid(row=1, column=0)
+
+        def get_avaliable_servers():
             try:
-                return game_servers.get(game_servers.curselection())
+                self.client.listen_channel.start_consuming()
             except TclError:
                 return
 
+        t = threading.Thread(target=get_avaliable_servers, args=())
+        t.setDaemon(True)
+        t.start()
+
+    def set_board_size(self):
+
+        board_size = Frame(self.root)
+        board_size.grid(row=0, column=0)
+
+        board_size_label = Label(board_size, text="Enter a board size", font=('times', 12))
+        board_size_label.grid(row=0, column=0)
+
+        board_size_entry = Entry(board_size)
+        board_size_entry.grid(row=0, column=1, pady=5)
+
+        def check_board_size():
+            board_size = board_size_entry.get()
+            try:
+                if 10 <= int(board_size) <= 40:
+                    self.root.destroy()
+                    self.client.set_type(1)
+                    self.client.send_type(None, board_size)
+                    root = Tk()
+                    board = Board(root, int(board_size), self.client)
+                    board.initShipBoard()
+                    board.initPositioning()
+                    root.mainloop()
+                else:
+                    tkMessageBox.showerror("Error!", "Board size must be less than 40 and bigger than 10 cells!")
+            except ValueError:
+                return
+
+        confirm_size = Button(board_size, text='OK!', command=check_board_size)
+        confirm_size.grid(row=1, column=0, sticky=W + E, columnspan=2)
+
+    def initUI(self):
+        # Return the chosen game server
+        def choose_game():
+            try:
+                session = game_servers.get(game_servers.curselection())
+                if "/" not in session:
+                    return
+                else:
+                    game_id, game_size = session.split("/")
+                    self.client.send_type(game_id)
+                    # self.root.destroy()
+                    # root = Tk()
+                    # board = Board(root, 10, self.client)
+                    # board.initShipBoard()
+                    # board.initPositioning()
+                    # root.mainloop()
+
+            except TclError:
+                return
 
         login_form = Frame(self.root)
         login_form.grid(row=0, column=0)
 
-        create_game = Button(login_form, text='Create New Game!', width=20, bg='light blue', font=('times', 14))
+
+        def new_game():
+            self.root.destroy()
+            root = Tk()
+            editor = Login(root)
+            editor.set_board_size()
+            root.mainloop()
+
+        create_game = Button(login_form, text='Create New Game!', width=20,
+                             bg='light blue', font=('times', 14), command=new_game)
         create_game.grid(row=0, column=1)
 
         join_game = Button(login_form, text='Join Game!', font=('times', 14), bg='light blue', command=choose_game)
@@ -30,9 +106,23 @@ class Login(Frame):
         game_servers = Listbox(login_form, width=20, height=10, font=10, bg='ghost white', activestyle='none')
         game_servers.grid(row=1, column=0)
 
-        # Sample
-        for item in ["one", "two", "three", "four"]:
-            game_servers.insert(END, item)
+        def get_game_sessions():
+            try:
+                while True:
+                    game_sessions = self.client.get_game_list()
+                    if game_sessions:
+                        for game_id, game_size in game_sessions:
+                            game_servers.insert(END, game_id + "/" + game_size)
+                    else:
+                        game_servers.insert(END, "No active games!")
+                    time.sleep(3)
+                    game_servers.delete(0, END)
+            except TclError:
+                return
+
+        t = threading.Thread(target=get_game_sessions, args=())
+        t.setDaemon(True)
+        t.start()
 
     def get_nickname(self):
         nickname_form = Frame(self.root)
@@ -45,18 +135,15 @@ class Login(Frame):
         nickname_entry.grid(row=0, column=1, pady=5)
 
         def check_nickname():
-            nickname = nickname_form.get()
-            if not nickname:
+            nickname = nickname_entry.get()
+            if not nickname or not self.client.isFreeName(nickname):
                 return
-            return nickname
+            self.initUI()
 
         nickname_button = Button(nickname_form, text='OK!', width=10, command=check_nickname)
         nickname_button.grid(row=1, column=0, sticky=E + W, columnspan=2)
 
-
-
-
 root = Tk()
 editor = Login(root)
+editor.choose_server()
 root.mainloop()
-
---
+++ battle_server.py
@@ -0,0 +1,225 @@
+import uuid
+import pika
+import threading
+
+serverID = uuid.uuid1()
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host='127.0.0.1', port=5672))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue_durable', durable=True)
+channel.queue_declare(queue='servers_queue', durable=True)
+
+
+def lifeCondition():
+    time = 2
+    response = str(serverID)
+    channel.basic_publish(exchange='',
+                          routing_key='servers_queue',
+                          properties=pika.BasicProperties(delivery_mode=2, expiration=str(int(time * 1000))),
+                          body=str(response))
+    threading.Timer(time, lifeCondition).start()
+
+
+lifeCondition()
+
+
+class GameSession:
+    def __init__(self, login, size):
+        self.id = uuid.uuid4()
+        self.ships = []
+        self.players = []
+        self.master_client = login
+        self.players.append(login)
+        self.state = 0
+        self.size = size
+
+    def disconnect(self, player_id):
+        return 0
+
+    def leave(self, player_id):
+        self.players[player_id] = 'Leaved'
+        return 0
+
+
+class Wait(GameSession):
+    def addPlayer(self, login):
+        self.players.append(login)
+
+    def addShipsOfPlayer(self, id, message):
+        ships = message.split('#')
+        for i in range(len(ships)):
+            entity = ships[i].split(',')
+            x = int(entity[0])
+            y = int(entity[1])
+            length = int(entity[2])
+            direction = entity[3]  # 0 - horizontal, 1 - vertical
+            coordinates = []
+            if direction == '0':
+                for j in range(length):
+                    coordinates.append((x, y + j))
+            elif direction == '1':
+                for j in range(length):
+                    coordinates.append((x + j, y))
+            ship = Ship(id, length, coordinates)
+            self.ships.append(ship)
+        return 0
+
+    def startGame(self):
+        return 0
+
+    def receiveStartGame(self):
+        self.startGame()
+
+
+class Game(GameSession):
+    def currentActivePlayers(self):
+        count = 0
+        for player in self.players:
+            if player.type == "Player":
+                count += 1
+        return count
+
+    def makeHit(self, login, coordinate):
+        hit_conditions = {}  # values: 0 - missed, 1 - hitted, 2 - sinked, 3 - hiter; keys: players_id
+        hit_conditions[login] = 3
+        for i in range(len(self.ships)):
+            if self.ships[i].owner_login != login and coordinate in self.ships[i].coordinates:
+                self.ships[i].coordinates.remove(coordinate)
+                if len(self.ships[i].coordinates) == 0:
+                    hit_conditions[self.ships[i].owner_login] = 2
+                    self.players[login].score += 2
+                else:
+                    hit_conditions[self.ships[i].owner_login] = 1
+                    self.players[login].score += 1
+        for player in self.players.keys():
+            if player not in hit_conditions.keys() and self.players[player].type == 'Player':
+                hit_conditions[player] = 0
+        return 0
+
+    def sendStats(self, hit_conditions, correlation_IDs):
+        messages = dict.fromkeys(self.players.keys(), '')
+        for player in self.players.keys():  # 3# + 0 - this player wasn't hitted, 1 - this player wasn't hitted, 2 - this player is spectator + # list of players which ships was sinked
+            if hit_conditions[player] == 0:
+                messages[player] += '#3#0#'
+            elif hit_conditions[player] == 1:
+                messages[player] += '#3#1#'
+        for player in hit_conditions:
+            if hit_conditions[player] == 2:
+                for p in self.players:
+                    messages[p] += player + ';'
+        for player in self.players.keys():
+            messages[player] = messages[player][:len(messages[player]) - 1] + '#'
+        for player in self.players.keys():
+            response = ''
+            if self.players[player].type == 'Player':
+                response = messages[player]
+            elif self.players[player].type == 'Spectator':
+                response = '#3#2'
+            correlation_id = correlation_IDs[player]
+            channel.basic_publish(exchange='',
+                                  routing_key='rpc_queue_durable',
+                                  properties=pika.BasicProperties(correlation_id=correlation_id, delivery_mode=2, ),
+                                  body=str(response))
+
+    def checkEndGame(self):
+        owner = self.ships[0].owner_login
+        for i in range(1, len(self.ships)):
+            if self.ships[i].owner_login != owner:
+                return False
+        return True
+
+
+class Ship:
+    def __init__(self, owner_login, length, coordinates):
+        self.length = length
+        self.coordinates = coordinates
+        self.owner_login = owner_login
+
+
+class Player:
+    def __init__(self, login, cor_id):
+        self.login = login
+        self.score = 0
+        self.type = 'Player'  # Player, Spectator, Leaved
+        self.corID = cor_id
+
+
+Players = {}
+CorrIDs = {}
+GameSessions = {}
+
+
+class Parser:
+    @staticmethod
+    def parse(request, cor_id):
+        print cor_id
+        subrequests = request.split('#')
+        if (len(subrequests) == 0):
+            return
+
+        if (subrequests[0] == '0'):
+            if (len(subrequests) < 2):
+                return '0#0'
+            request_name = subrequests[1]
+            if (request_name in Players.keys()):
+                response = '0#0'
+                return response
+            Players[request_name] = Player(request_name, cor_id)
+            CorrIDs[cor_id] = request_name
+            response = '0#1'
+            return response
+
+        if (subrequests[0] == '1'):
+            response = '1#'
+            response_tail = ''
+            numOfActiveGames = 0
+            for game in GameSessions:
+                print game.state
+                if (game.state == 0):
+                    numOfActiveGames += 1
+                    response_tail += str(game.id) + '#'
+                    response_tail += str(game.size) + '#'
+                    response_tail += str(len(game.players)) + '#'
+            return response + str(numOfActiveGames) + '#' + response_tail
+
+        if (subrequests[0] == '2'):
+            print request
+            if (len(subrequests) < 3):
+                return '2#0'
+            if (subrequests[1] == '0'):
+                #master_login = CorrIDs[cor_id]
+                #print master_login
+                #game_size = subrequests[2]
+                #newGame = GameSession(master_login, game_size)
+                #GameSessions[newGame.id] = newGame
+                return '2#1'
+            player_login = CorrIDs[cor_id]
+            requested_game = subrequests[2]
+            if (GameSessions[requested_game].state == 1):
+                return '2#0'
+            GameSessions[requested_game].addPlayer(player_login)
+            return '2#1'
+
+
+def on_request(ch, method, props, body):
+    request = str(body)
+
+    response = Parser.parse(request, props.correlation_id)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id= \
+                                                         props.correlation_id,
+                                                     delivery_mode=2, ),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue_durable')
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
