b3fd0abc4750b3199890027125578ede526a4c1c
yevhen-tyshchenko <tyshchenko.z@gmail.com>
2016-12-13 15:20:09 +0200

    Fixed random generation

--- ships_generator.py
+++ ships_generator.py
@@ -1,44 +1,83 @@
 import random
 
-def generate_ships(board_size):
-    board = [[0]*board_size for i in range(board_size)]
-    available_positions = [[i, j] for i in xrange(board_size) for j in xrange(board_size)]
-    ship_types = [5, 4, 3, 3, 2]
-    ships = []
-    while ship_types:
-        ship_length = ship_types[0]
-        horizontal = random.getrandbits(1)
-        if bool(horizontal):
-            position_x = random.randint(0, board_size-1)
-            position_y = random.randint(0, board_size-ship_length-1)
-            pairs = []
-            for s in range(position_y-1, position_y+ship_length+1):
-                for p in range(position_x-1, position_x+2):
-                    if 0 <= s < board_size and 0 <= p < board_size:
-                        pairs.append([p, s])
-            if all(pairs[i] in available_positions for i in range(len(pairs))):
-                for i in range(ship_length):
-                    board[position_x][position_y+i] = 1
-                    i += 1
-                print horizontal
-                [available_positions.remove(pairs[i]) for i in range(len(pairs))]
-                ship_types.remove(ship_types[0])
-                ships.append(tuple(map(str, (position_x, position_y, ship_length, int(horizontal)))))
-        else:
-            position_x = random.randint(0, board_size-ship_length-1)
-            position_y = random.randint(0, board_size-1)
-            pairs = []
-            for s in range(position_x-1, position_x+ship_length+1):
-                for p in range(position_y-1, position_y+2):
-                    if 0 <= s < board_size and 0 <= p < board_size:
-                        pairs.append([s, p])
-            if all(pairs[i] in available_positions for i in range(len(pairs))):
-                for i in range(ship_length):
-                    board[position_x+i][position_y] = 1
-                    i += 1
-                print horizontal
-                [available_positions.remove(pairs[i]) for i in range(len(pairs))]
-                ship_types.remove(ship_types[0])
-                ships.append(tuple(map(str, (position_x, position_y, ship_length, int(horizontal)))))
-    return ships
+def generate_random_fleet(board_size):
+    ships = [5, 4, 3, 3, 2]
+    placed_ships = []
+    board = []
+    for i in range(board_size):
+        board_row = []
+        for j in range(board_size):
+            board_row.append(0)
+        board.append(board_row)
 
+    for ship in ships:
+        valid = False
+        while (not valid):
+
+            x = random.randint(1, board_size)-1
+            y = random.randint(1, board_size)-1
+            o = random.randint(0, 1)
+
+            if o == 0:
+                ori = "v"
+            else:
+                ori = "h"
+            valid = validate(board, ship, x, y, ori)
+
+        board = place_ship(board, ship, ori, x, y)
+        if ori == "v":
+            placed_ships.append(tuple(map(str, (x, y, ship, 0))))
+        elif ori == "h":
+            placed_ships.append(tuple(map(str, (x, y, ship, 1))))
+
+    return placed_ships
+
+
+def place_ship(board, ship, ori, x, y):
+
+    if ori == "v":
+        for i in range(ship):
+            board[x + i][y] = 1
+
+        for i in range(-1, ship+1):
+            for j in [-1, 0, 1]:
+                if 0 <= x+i < len(board) and 0 <= y+j < len(board):
+                    board[x+i][y+j] = 1
+
+    elif ori == "h":
+        for i in range(ship):
+            board[x][y + i] = 1
+
+        for i in range(-1, ship+1):
+            for j in [-1, 0, 1]:
+                if 0 <= x+j < len(board) and 0 <= y+i < len(board):
+                    board[x+j][y+i] = 1
+    return board
+
+
+def validate(board, ship, x, y, ori):
+
+    if ori == "v" and x + ship > len(board):
+        return False
+    elif ori == "h" and y + ship > len(board):
+        return False
+    else:
+        if ori == "v":
+            try:
+                for i in range(-1, ship+1):
+                    for j in [-1, 0, 1]:
+                        if 0 <= x + i < len(board) and 0 <= y + j < len(board):
+                            if board[x+i][y+j] != 0:
+                                return False
+            except IndexError:
+                pass
+        elif ori == "h":
+            try:
+                for i in range(-1, ship+1):
+                    for j in [-1, 0, 1]:
+                        if 0 <= x + j < len(board) and 0 <= y + i < len(board):
+                            if board[x+j][y+i] != 0:
+                                return False
+            except IndexError:
+                pass
+    return True
