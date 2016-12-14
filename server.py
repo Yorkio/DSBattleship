988c4f4baab694344db14877252311ca16d20644
Yorkio <yurii.toma@gmail.com>
2016-12-14 09:36:52 +0200

    request 5 bug fixes

--- server.py
+++ server.py
@@ -254,7 +254,10 @@ class Parser:
 
         if (subrequests[0] == '5'):
             game_session = PlayerGame[cor_id]
-            return '5#' + str(GameSessions[game_session].players[GameSessions[game_session].curActive])
+            if (not game_session):
+                return '5#-1'
+            return '5#' + str(GameSessions[game_session].curActive)
+            #return '5#' + str(GameSessions[game_session].players[GameSessions[game_session].curAct])
 
         if (subrequests[0] == '6'):
             game_session = PlayerGame[cor_id]
